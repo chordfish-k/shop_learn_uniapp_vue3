c673560a16dc04f0ebcab6830126b827bd38c1e0
chordfish <592229466@qq.com>
2023-08-18 13:08:13 +0800

    feat: 商品详情-数据获取与渲染-轮播图交互

---
+++ src/services/goods.ts
@@ -0,0 +1,20 @@
+// 商品详情
+// GET/goods
+// 规格集合一定要和skus集合下的specs 顺序保持一致
+
+import type { GoodsResult } from "@/types/goods"
+import { http } from "@/utils/http"
+
+/**
+ * 商品详情
+ * @param id 商品id
+ */
+export const getGoodsByIdAPI = (id: string) => {
+    return http<GoodsResult>({
+        method: 'GET',
+        url: '/goods',
+        data: {
+            id,
+        }
+    })
+}
