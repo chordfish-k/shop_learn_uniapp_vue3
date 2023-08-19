280715e2e7e84d2a81e97e4a564be62622067129
chordfish <592229466@qq.com>
2023-08-19 14:18:47 +0800

    feat: 会员中心-更新Store信息

--- src/pagesMember/profile/profile.vue
+++ src/pagesMember/profile/profile.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { getMemberProfileAPI, putMemberProfileAPI } from '@/services/profile';
+import { useMemberStore } from '@/stores';
 import type { ProfileDetail } from '@/types/member';
 import { onLoad } from '@dcloudio/uni-app';
 import { ref } from 'vue';
@@ -19,6 +20,8 @@ onLoad(() => {
   getMemberProfileData()
 })
 
+const memberStore = useMemberStore()
+
 // 修改头像
 const onAvatarChange = () => {
   // 调用拍照/选择图片
@@ -35,7 +38,10 @@ const onAvatarChange = () => {
         success: (res) => {
           if (res.statusCode === 200) {
             const avatar = JSON.parse(res.data).result.avatar
+            // 个人信息页数据更新
             profile.value!.avatar = avatar
+            // Store头像更新
+            memberStore.profile!.avatar = avatar
             uni.showToast({ icon: 'success', title: '更新成功' })
           } else {
             uni.showToast({ icon: 'error', title: '更新失败' })
@@ -51,7 +57,14 @@ const onSubmit = async () => {
   const res = await putMemberProfileAPI({
     nickname: profile.value?.nickname,
   })
+  // 更新Store昵称
+  memberStore.profile!.nickname = res.result.nickname
   uni.showToast({ icon: 'success', title: '保存成功' })
+  // 返回上一页
+  setTimeout(() => {
+    uni.navigateBack()
+  }, 400)
+
 }
 </script>
 
